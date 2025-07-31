80ea7f202d73202418f82b909fb26c11894b39e6
Stuart Watkinson <Stuwat95@gmail.com>
2025-07-31 20:35:00 +0100

    Rough Draft of Text Spawn system

---
+++ Assets/Scripting/TextSpawner.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Class controls the text spawning on randomly screen
+/// </summary>
+public class TextSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject m_text;
+    private Coroutine m_spawnTimer;
+    private List<GameObject> m_currentScreenText = new List<GameObject>();
+    private int m_timerTest = 0;
+
+    private void SpawnText(GameObject text)
+    {
+        Vector2 position = new Vector2(Random.Range(-5.0f, 5.0f), Random.Range(-5.0f, 5.0f));
+        m_currentScreenText.Add(Instantiate(text, position, Quaternion.identity));
+    }
+
+    private void DestroyText()
+    {
+        for (int i = 0; i < m_currentScreenText.Count; i++)
+        {
+            Destroy(m_currentScreenText[i]);
+        }
+    }
+
+    private IEnumerator SpawnTimer()
+    {
+        if (m_timerTest < 10)
+        {
+            SpawnText(m_text);
+            yield return new WaitForSeconds(1);
+            m_timerTest++;
+        }
+        else
+        {
+            DestroyText();
+            m_timerTest = 0;
+            m_currentScreenText.Clear();
+        }
+        StartCoroutine(SpawnTimer());
+    }
+
+    private void Start()
+    {
+        StartCoroutine(SpawnTimer());
+    }
+
+}
